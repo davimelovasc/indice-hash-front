8e8f185c9e9494a4cbbd90a76722408851d707a3
Davi Vasconcelos <davimelovasc@gmail.com>
2020-06-16 19:34:35 -0300

    last commit

--- av2.js
+++ av2.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
 
+    $.ajax({
+        type: 'GET',
+        url: 'http://localhost:8080/',
+        contentType:"application/json; charset=utf-8"
+    })
+
     $("#executeQuery").click(function(ev) {
 
         let query = $("#queryInput").val()
@@ -11,39 +17,35 @@ $(document).ready(function() {
         contentType:"application/json; charset=utf-8",
         data: query,
         success: function(msg){
-            let tabelas = msg[2];
+            if(msg[3]) {
+                createGraphWithWhere(msg)
+            } else {
+                createGraphWithoutWhere(msg)
+            }
+            console.log(msg[2])
+            getTable(msg[2])
 
+
+            
         }
     });
 
     })
 
+    function createGraphWithWhere(msg) {
+        let nodes = new vis.DataSet([
+            {id: 1, label: "Tabela " + msg[2]}, 
+            {id: 3, label: "Projeção [" + msg[1] + "]"},
+            {id: 2, label: msg[3]}
+        ])
 
-    function initGraph(tables) {
-        /* let nodes = new vis.DataSet([])
-        tables.forEach(element => {
-            nodes.push({id: 0, element})
-        }); */
-        
-        var nodes = new vis.DataSet([
-            {id: 1, label: 'Node 1'},
-            {id: 2, label: 'Node 2'},
-            {id: 3, label: 'Node 3'},
-            {id: 4, label: 'Node 4'},
-            {id: 5, label: 'Node 5'}
-        ]);
-    
-        // create an array with edges
         var edges = new vis.DataSet([
-            {from: 1, to: 3},
             {from: 1, to: 2},
-            {from: 2, to: 4},
-            {from: 2, to: 5}
-        ]);
-    
-        // create a network
-        var container = document.getElementById('grafo');
-    
+            {from: 2, to: 3}
+        ])
+
+        var container = document.getElementById('grafo')
+
         // provide the data in the vis format
         var data = {
             nodes: nodes,
@@ -52,7 +54,61 @@ $(document).ready(function() {
         var options = {};
     
         // initialize your network!
-        var network = new vis.Network(container, data, options);
+        var network = new vis.Network(container, data, options)
     }
+
+    function createGraphWithoutWhere(msg) {
+        let nodes = new vis.DataSet([
+            {id: 1, label: "Tabela " + msg[2]}, 
+            {id: 2, label: "Projeção [" + msg[1] + "]"}
+        ])
+
+        var edges = new vis.DataSet([
+            {from: 1, to: 2}
+        ])
+
+        var container = document.getElementById('grafo');
+
+        // provide the data in the vis format
+        var data = {
+            nodes: nodes,
+            edges: edges
+        };
+        var options = {};
+    
+        // initialize your network!
+        var network = new vis.Network(container, data, options)
+    }
+
+    function getTable(tableName) {
+        $.ajax({
+            type: 'GET',
+            url: 'http://localhost:8080/table/' + tableName,
+            contentType:"application/json; charset=utf-8",
+            success: function(records) {
+                let table = records
+                fillTable(table)
+            }
+        })
+    }
+
+    function fillTable(table) {
+        if(table == "empregado") {
+            console.log("entrou")
+            console.log(table)
+            table['tuplas'].forEach(function(val) {
+                $("#empregadoTable").append(`<tr>
+                    <td>${val['matri']}</td>
+                    <td>${val['nome']}</td>
+                    <td>${val['salario']}</td>
+                </tr>
+                `)
+            })
+            $("#empregadoTable").removeClass("d-none")
+        }
+
+    }
+
+   
 
 })
